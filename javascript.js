d1bf6479e4bf95ef3b651d0c130078241be7ec83
Filip Baotić <6832343+filipbaotic@users.noreply.github.com>
2023-04-24 00:20:30 +0200

    napisani projekt

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function prikaziFran() {
+  var x = document.getElementById("fran");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function prikaziFilip() {
+  var x = document.getElementById("filip");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
